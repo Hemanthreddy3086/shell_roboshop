f6961a081a8b9e41ff55015b97a5f8fb65592299
sunil <sunil@gmail.com>
2026-02-07 12:57:37 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -4,7 +4,7 @@ SG_ID="sg-047414b5251141a52"
 AMI_ID="ami-0220d79f3f480ecf5"
 # Create a new EC2 instance
 
-for instance i $@
+for instance in $@
 do  
    INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
@@ -34,19 +34,3 @@ do
 done
 
 
-#!/bin/bash
-
-NUM1=100
-NUM2=200
-
-SUM=$(($NUM1+$NUM2))
-
-echo "Sum is: $SUM"
-
-# Array
-FRUITS=("Apple" "Banana" "Pomo")
-
-echo "Fruits are: ${FRUITS[@]}"
-echo "First Fruit is: ${FRUITS[0]}"
-echo "Second Fruit is: ${FRUITS[1]}"
-echo "Third Fruit is: ${FRUITS[2]}"
