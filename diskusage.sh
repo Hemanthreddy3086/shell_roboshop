c4f69e5bdc342e3e1b7ec1ee57aa32a9c06885a5
sunil <sunil@gmail.com>
2026-02-10 13:44:55 +0530

    actions

--- diskusage.sh
+++ diskusage.sh
@@ -20,7 +20,7 @@ do
     PARTITION=$(echo $line | awk '{print $7}')
 
     if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
-        MESSAGE+="High Disk usage on <b>$PARTITION</b>: <b>$USAGE%</b><br>"
+        MESSAGE+="High Disk usage on $PARTITION:$USAGE%<br>"
     fi
 done <<< $DISK_USAGE
 
