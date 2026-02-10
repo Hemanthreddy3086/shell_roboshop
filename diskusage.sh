b8eeb447faefbad7d73dee2868e00fdb62dd93aa
sunil <sunil@gmail.com>
2026-02-10 13:11:12 +0530

    actions

--- diskusage.sh
+++ diskusage.sh
@@ -5,6 +5,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 MESSAGE=""
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 log(){
     echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1"
@@ -25,4 +26,4 @@ done <<< $DISK_USAGE
 
 echo -e "$MESSAGE"
 
-#sh mail.sh "info@joindevops.com" "High Disk Usage Alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
+sh mail.sh "hemanthreddy0635.com" "High Disk Usage Alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
